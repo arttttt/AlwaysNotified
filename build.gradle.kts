fb708f1c5b30792018e6e719be55a7c6c3117d77
arttttt <artem-bambalov@yandex.ru>
2023-10-23 22:18:12 +0300

    add base CI script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.parsers.DocumentBuilderFactory
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
@@ -8,3 +12,19 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
+
+tasks.register("printVersion") {
+    doLast {
+        val manifestFile = file("app/build/intermediates/merged_manifests/release/AndroidManifest.xml").readText()
+        val xml = DocumentBuilderFactory
+            .newInstance()
+            .newDocumentBuilder()
+            .parse(InputSource(StringReader(manifestFile)))
+
+        val versionCode = xml.documentElement.getAttribute("android:versionCode")
+        val versionName = xml.documentElement.getAttribute("android:versionName")
+
+        println("Version Name: $versionName")
+        println("Version Code: $versionCode")
+    }
+}
