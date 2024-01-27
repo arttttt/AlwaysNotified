7e730ec4f296d0cef43d2b3c2fa8d2bf5cdf9c19
arttttt <artem-bambalov@yandex.ru>
2024-01-27 15:17:10 +0200

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,12 @@ import org.xml.sax.InputSource
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
 
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlinSerialization) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
 
 tasks.register("printVersion") {
     doLast {
