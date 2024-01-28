191934a0e0d65ca50adf0c6f6a3c893dcf326af3
arttttt <artem-bambalov@yandex.ru>
2024-01-28 13:02:06 +0200

    bump up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.arttttt.alwaysnotified"
         minSdk = 33
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "0.0.2"
 
         vectorDrawables {
             useSupportLibrary = true
