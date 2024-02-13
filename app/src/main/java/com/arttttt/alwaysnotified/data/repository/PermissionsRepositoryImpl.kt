0dee50192e42aef810e8519d73851528a0f43967
arttttt <artem-bambalov@yandex.ru>
2024-02-13 21:30:21 +0200

    clean imports

--- app/src/main/java/com/arttttt/alwaysnotified/data/repository/PermissionsRepositoryImpl.kt
+++ app/src/main/java/com/arttttt/alwaysnotified/data/repository/PermissionsRepositoryImpl.kt
@@ -1,9 +1,6 @@
 package com.arttttt.alwaysnotified.data.repository
 
 import android.content.Context
-import com.arttttt.alwaysnotified.data.model.IgnoreBatteryOptimizationsPermission
-import com.arttttt.alwaysnotified.data.model.PostNotificationsPermission
-import com.arttttt.alwaysnotified.data.model.QueryAllPackagesPermission
 import com.arttttt.alwaysnotified.domain.entity.permission.Permission2
 import com.arttttt.alwaysnotified.domain.repository.PermissionsRepository
 
