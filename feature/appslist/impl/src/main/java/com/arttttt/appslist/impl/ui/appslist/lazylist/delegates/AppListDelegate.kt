848cb01df234880f4cf6da5d840023ec8c726a7c
arttttt <artem-bambalov@yandex.ru>
2024-04-20 21:07:46 +0300

    correct spacer

--- feature/appslist/impl/src/main/java/com/arttttt/appslist/impl/ui/appslist/lazylist/delegates/AppListDelegate.kt
+++ feature/appslist/impl/src/main/java/com/arttttt/appslist/impl/ui/appslist/lazylist/delegates/AppListDelegate.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -68,6 +69,8 @@ internal fun AppListDelegate(
         }
 
         if (item.selectedActivityTitle != null) {
+            Spacer(modifier = Modifier.height(4.dp))
+
             Text(
                 text = "Selected activity: ${item.selectedActivityTitle}",
                 fontSize = 12.sp,
