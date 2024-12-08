1cf1cac49ea8012435ce817b68314be65b21ccc9
arttttt <artem-bambalov@yandex.ru>
2024-12-08 02:38:06 +0200

    move actor to a separated file

--- feature/appslist/impl/src/main/java/com/arttttt/appslist/impl/domain/store/AppsStore.kt
+++ feature/appslist/impl/src/main/java/com/arttttt/appslist/impl/domain/store/AppsStore.kt
@@ -1,19 +1,11 @@
 package com.arttttt.appslist.impl.domain.store
 
 import com.arttttt.appslist.SelectedActivity
-import com.arttttt.appslist.impl.domain.entity.ActivityInfo
 import com.arttttt.appslist.impl.domain.entity.AppInfo
 import com.arttttt.appslist.impl.domain.repository.AppsRepository
-import com.arttttt.simplemvi.actor.ActorScope
-import com.arttttt.simplemvi.actor.dsl.DslActorScope
-import com.arttttt.simplemvi.actor.dsl.actorDsl
 import com.arttttt.simplemvi.store.Store
 import com.arttttt.simplemvi.store.createStore
 import com.arttttt.simplemvi.store.storeName
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.joinAll
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 internal class AppsStore(
     private val appsRepository: AppsRepository,
@@ -24,45 +16,12 @@ internal class AppsStore(
         selectedActivities = null,
         isInProgress = false
     ),
-    initialIntents = listOf(
-        Intent.GetInstalledApplications,
-    ),
-    actor = actorDsl {
-            onIntent<Intent.GetInstalledApplications> {
-                launch {
-                    reduce {
-                        copy(
-                            isInProgress = true,
-                        )
-                    }
-
-                    joinAll(
-                        launch { getInstalledApplications(appsRepository) },
-                        launch { getSelectedActivities(appsRepository) },
-                    )
-                }
-                    .invokeOnCompletion {
-                        reduce {
-                            copy(
-                                isInProgress = false,
-                            )
-                        }
-                    }
-            }
-
-            onIntent<Intent.SetSelectedActivity> { intent ->
-                setSelectedActivity(
-                    pkg = intent.pkg,
-                    selectedActivity = intent.selectedActivity,
-                    appsRepository = appsRepository,
-                )
-            }
-        },
+    actor = AppsStoreActor(
+        appsRepository = appsRepository,
+    )
 ) {
 
     sealed interface Intent {
-
-        data object GetInstalledApplications : Intent
 
         data class SetSelectedActivity(
             val pkg: String,
@@ -79,89 +38,5 @@ internal class AppsStore(
     sealed interface SideEffect {
 
         data object ActivitiesChanged : SideEffect
-    }
-}
-
-private suspend fun ActorScope<AppsStore.Intent, AppsStore.State, AppsStore.SideEffect>.getInstalledApplications(
-    appsRepository: AppsRepository,
-) {
-    val applications = withContext(Dispatchers.IO) {
-        appsRepository
-            .getInstalledApplications()
-            .map { info ->
-                info.copy(
-                    activities = info
-                        .activities
-                        .sortedBy(ActivityInfo::title)
-                        .toSet()
-                )
-            }
-            .sortedBy { info -> info.title }
-            .associateBy { info -> info.pkg }
-    }
-
-    reduce {
-        copy(
-            applications = applications,
-        )
-    }
-}
-
-private suspend fun ActorScope<AppsStore.Intent, AppsStore.State, AppsStore.SideEffect>.getSelectedActivities(
-    appsRepository: AppsRepository,
-) {
-    val selectedActivities = appsRepository
-        .getSelectedApps()
-        .associateBy { activity ->
-            activity.pkg
-        }
-        .mapValues { (pkg, value) ->
-            SelectedActivity(
-                pkg = pkg,
-                name = value.name,
-                manualMode = value.manualMode,
-            )
-        }
-
-    reduce {
-        copy(
-            selectedActivities = selectedActivities,
-        )
-    }
-}
-
-private fun DslActorScope<AppsStore.Intent, AppsStore.State, AppsStore.SideEffect>.setSelectedActivity(
-    pkg: String,
-    selectedActivity: SelectedActivity?,
-    appsRepository: AppsRepository,
-) {
-    launch {
-        val selectedActivities = withContext(Dispatchers.IO) {
-            state
-                .selectedActivities
-                .let { activities ->
-                    val mutableActivities = activities?.toMutableMap() ?: mutableMapOf()
-
-                    if (selectedActivity == null) {
-                        if (mutableActivities.contains(pkg)) {
-                            appsRepository.removeActivity(mutableActivities.getValue(pkg))
-                        }
-                        mutableActivities.remove(pkg)
-                    } else {
-                        mutableActivities[pkg] = selectedActivity
-                        appsRepository.saveActivity(selectedActivity)
-                    }
-
-                    mutableActivities.toMap()
-                }
-        }
-
-        reduce {
-            copy(
-                selectedActivities = selectedActivities,
-            )
-        }
-
-        sideEffect(AppsStore.SideEffect.ActivitiesChanged)
     }
 }
---
+++ feature/appslist/impl/src/main/java/com/arttttt/appslist/impl/domain/store/AppsStoreActor.kt
@@ -0,0 +1,125 @@
+package com.arttttt.appslist.impl.domain.store
+
+import com.arttttt.appslist.SelectedActivity
+import com.arttttt.appslist.impl.domain.entity.ActivityInfo
+import com.arttttt.appslist.impl.domain.repository.AppsRepository
+import com.arttttt.simplemvi.actor.DefaultActor
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+internal class AppsStoreActor(
+    private val appsRepository: AppsRepository,
+) : DefaultActor<AppsStore.Intent, AppsStore.State, AppsStore.SideEffect>() {
+
+    override fun onInit() {
+        scope.launch {
+            reduce {
+                copy(
+                    isInProgress = true,
+                )
+            }
+
+            joinAll(
+                launch { getInstalledApplications() },
+                launch { getSelectedActivities() },
+            )
+        }
+            .invokeOnCompletion {
+                reduce {
+                    copy(
+                        isInProgress = false,
+                    )
+                }
+            }
+    }
+
+    override fun handleIntent(intent: AppsStore.Intent) {
+        when (intent) {
+            is AppsStore.Intent.SetSelectedActivity -> setSelectedActivity(
+                pkg = intent.pkg,
+                selectedActivity = intent.selectedActivity,
+            )
+        }
+    }
+
+    private suspend fun getInstalledApplications() {
+        val applications = withContext(Dispatchers.IO) {
+            appsRepository
+                .getInstalledApplications()
+                .map { info ->
+                    info.copy(
+                        activities = info
+                            .activities
+                            .sortedBy(ActivityInfo::title)
+                            .toSet()
+                    )
+                }
+                .sortedBy { info -> info.title }
+                .associateBy { info -> info.pkg }
+        }
+
+        reduce {
+            copy(
+                applications = applications,
+            )
+        }
+    }
+
+    private suspend fun getSelectedActivities() {
+        val selectedActivities = appsRepository
+            .getSelectedApps()
+            .associateBy { activity ->
+                activity.pkg
+            }
+            .mapValues { (pkg, value) ->
+                SelectedActivity(
+                    pkg = pkg,
+                    name = value.name,
+                    manualMode = value.manualMode,
+                )
+            }
+
+        reduce {
+            copy(
+                selectedActivities = selectedActivities,
+            )
+        }
+    }
+
+    private fun setSelectedActivity(
+        pkg: String,
+        selectedActivity: SelectedActivity?,
+    ) {
+        scope.launch {
+            val selectedActivities = withContext(Dispatchers.IO) {
+                state
+                    .selectedActivities
+                    .let { activities ->
+                        val mutableActivities = activities?.toMutableMap() ?: mutableMapOf()
+
+                        if (selectedActivity == null) {
+                            if (mutableActivities.contains(pkg)) {
+                                appsRepository.removeActivity(mutableActivities.getValue(pkg))
+                            }
+                            mutableActivities.remove(pkg)
+                        } else {
+                            mutableActivities[pkg] = selectedActivity
+                            appsRepository.saveActivity(selectedActivity)
+                        }
+
+                        mutableActivities.toMap()
+                    }
+            }
+
+            reduce {
+                copy(
+                    selectedActivities = selectedActivities,
+                )
+            }
+
+            sideEffect(AppsStore.SideEffect.ActivitiesChanged)
+        }
+    }
+}
